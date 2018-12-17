7d78747e510c00c6df77f851611d7f6a710e10e9
Andy Schneider <andyschndr@gmail.com>
2018-12-17 05:59:28 -0700

    Add Power duty cycling, median filtering
    Added a whole rigmarole for acquiring samples at a certain rate, and
    turning the lidar off otherwise. This seems to work pretty well,
    although at the few distances I tested it seems like there was up to a
    20cm offset (measuring 147cm when it should have measured 167). However,
    given that we're looking for relative changes in distance measured, this
    should work pretty well anyway.
    
    Speaking of that, this adds some functionality for median filtering
    samples, and setting a flag when we detect a significant change in the
    filtered distance. The most recent filtered distance and the flag are
    accessible by public functions.

--- nrf_firmware/src/sensors/lidar/lidar_pub.h
+++ nrf_firmware/src/sensors/lidar/lidar_pub.h
@@ -12,11 +12,14 @@ extern "C" {
 /**********************************************************
                         INCLUDES
 **********************************************************/
-#include "ant_stack_handler_types.h"
+#include "app_error.h"
+
 
 /**********************************************************
                         CONSTANTS
 **********************************************************/
+
+#define LIDAR_MAX_DISTANCE 4000     // cm
 
 /**********************************************************
                        DECLARATIONS
@@ -26,16 +29,65 @@ extern "C" {
                        DEFINITIONS
 **********************************************************/
 
+
+/**
+ * @brief Different sampling rates for the LIDAR
+ */
+typedef enum
+{
+    LIDAR_SAMPLE_RATE_20Hz,
+    LIDAR_SAMPLE_RATE_10Hz, ///< Default.
+    LIDAR_SAMPLE_RATE_5Hz,
+    LIDAR_SAMPLE_RATE_4Hz,
+    LIDAR_SAMPLE_RATE_2Hz,
+    LIDAR_SAMPLE_RATE_1Hz,
+    LIDAR_SAMPLE_RATE_COUNT,
+} lidar_sample_rate_type_t;
+
+/**
+ * @brief Different sampling rates for the LIDAR
+ */
+typedef enum
+{
+    LIDAR_ERROR_NONE,          ///< No Error.
+    LIDAR_ERROR_SAMPLING_FAIL,
+    LIDAR_ERROR_CODE_COUNT,
+} lidar_error_code_type_t;
+
 /**
  * @brief
  */
 void lidar_update_main(void);
+
+
+/**
+ * @brief Set the sample rate!
+ */
+void lidar_set_sample_rate(lidar_sample_rate_type_t rate);
+
+/**
+ * @brief Check if there's been an alarming change in distance recently
+ *
+ */
+bool lidar_get_distance_change_flag(void);
+
+/**
+ * @brief Get the most recent median-filtered sample from the lidar.
+ *
+ */
+uint16_t lidar_get_filtered_distance(void);
 
 /**
  * @brief
  *
  */
 void lidar_init(void);
+
+/**
+ * @brief
+ *
+ */
+void lidar_deinit(void);
 
 #ifdef __cplusplus
 }
