8116e0e60273e415548b33e9cb65fecfbd4a3156
Patrick Settle <patricksettle@hotmail.com>
2018-12-27 01:51:53 -0700

    Configuration UI started

---
+++ config_app/MissMooseConfigurationApplication/MissMooseConfigurationApplication/UIComponents/SensorNode.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MissMooseConfigurationApplication
+{
+    public enum HardwareConfiguration
+    {
+        HARDWARECONFIGURATION_2_PIR,
+        HARDWARECONFIGURATION_1_PIR_1_LIDAR,
+        HARDWARECONFIGURATION_1_PIR_1_LIDAR_LEDS,
+    }
+
+    public enum LedColour
+    {
+        LEDCOLOUR_RED,
+        LEDCOLOUR_YELLOW,
+        LEDCOLOUR_GREEN
+    }
+
+    public enum StatusColour
+    {
+        STATUSCOLOUR_RED,
+        STATUSCOLOUR_YELLOW,
+        STATUSCOLOUR_BLUE
+    }
+
+    public enum NodeRotation
+    {
+        NODEROTATION_0,
+        NODEROTATION_45,
+        NODEROTATION_90,
+        NODEROTATION_135,
+        NODEROTATION_180,
+        NODEROTATION_225,
+        NODEROTATION_270,
+        NODEROTATION_315
+    }
+
+    /// <summary>
+    /// Interaction logic for SensorNode.xaml
+    /// </summary>
+    public partial class SensorNode : UserControl
+    {
+        internal NodeRotation rotation = NodeRotation.NODEROTATION_0;
+        HardwareConfiguration configuration;
+        internal int NodeID;
+
+        public SensorNode(HardwareConfiguration configuration, int NodeID)
+        {
+            InitializeComponent();
+
+            this.configuration = configuration;
+            this.NodeID = NodeID;
+
+            switch(configuration)
+            {
+                case HardwareConfiguration.HARDWARECONFIGURATION_2_PIR:
+                    PIR_0_Deg.Visibility = Visibility.Visible;
+                    PIR_270_Deg.Visibility = Visibility.Visible;
+                    Lidar_270_Deg.Visibility = Visibility.Hidden;
+                    break;
+                case HardwareConfiguration.HARDWARECONFIGURATION_1_PIR_1_LIDAR:
+                case HardwareConfiguration.HARDWARECONFIGURATION_1_PIR_1_LIDAR_LEDS:
+                    PIR_0_Deg.Visibility = Visibility.Visible;
+                    PIR_270_Deg.Visibility = Visibility.Hidden;
+                    Lidar_270_Deg.Visibility = Visibility.Visible;
+                    break;
+            }
+
+            SetStatusColour(StatusColour.STATUSCOLOUR_BLUE);
+            SetLedColour(LedColour.LEDCOLOUR_GREEN);
+        }
+
+        public void SetLedColour(LedColour colour)
+        {
+            if(configuration == HardwareConfiguration.HARDWARECONFIGURATION_1_PIR_1_LIDAR_LEDS)
+            {
+                switch(colour)
+                {
+                    case LedColour.LEDCOLOUR_GREEN:
+                        OuterRing.Fill = Brushes.Green;
+                        break;
+                    case LedColour.LEDCOLOUR_RED:
+                        OuterRing.Fill = Brushes.Red;
+                        break;
+                    case LedColour.LEDCOLOUR_YELLOW:
+                        OuterRing.Fill = Brushes.Yellow;
+                        break;
+                }
+            }
+        }
+
+        public void SetStatusColour(StatusColour colour)
+        {
+            SolidColorBrush finalColour;
+
+            switch(colour)
+            {
+                case StatusColour.STATUSCOLOUR_BLUE:
+                    finalColour = Brushes.Blue;
+                    break;
+                case StatusColour.STATUSCOLOUR_YELLOW:
+                    finalColour = Brushes.Yellow;
+                    break;
+                case StatusColour.STATUSCOLOUR_RED:
+                    finalColour = Brushes.Red;
+                    break;
+                default:
+                    throw new InvalidOperationException("Unknown status colour");
+            }
+
+            switch(configuration)
+            {
+                case HardwareConfiguration.HARDWARECONFIGURATION_1_PIR_1_LIDAR:
+                    OuterRing.Fill = finalColour;
+                    break;
+                case HardwareConfiguration.HARDWARECONFIGURATION_2_PIR:
+                    OuterRing.Fill = finalColour;
+                    break;
+                case HardwareConfiguration.HARDWARECONFIGURATION_1_PIR_1_LIDAR_LEDS:
+                    InnerRing.Fill = finalColour;
+                    break;
+                default:
+                    throw new InvalidOperationException("Unknown hardware configuration");
+            }
+        }
+
+        public void SetRotation(NodeRotation rotation)
+        {
+            this.rotation = rotation;
+        }
+
+    }
+}
--- config_app/MissMooseConfigurationApplication/MissMooseConfigurationApplication/UIPages/ConfigurationPage.xaml.cs
+++ config_app/MissMooseConfigurationApplication/MissMooseConfigurationApplication/UIPages/ConfigurationPage.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MissMooseConfigurationApplication.UIPages
 {
@@ -20,9 +11,370 @@ namespace MissMooseConfigurationApplication.UIPages
     /// </summary>
     public partial class ConfigurationPage : Page
     {
+        #region Private Members
+        private List<List<PulsingCircle>> pulses;
+        private List<List<Viewbox>> sensorViewboxes;
+
+        private const int GridSize = 3;
+
+        private Viewbox ActiveViewbox = null;
+        #endregion
+
+        #region Public Methods
+
         public ConfigurationPage()
         {
             InitializeComponent();
+            InitializeViewboxes();
+            InitializePulses();
+            
+
+            AddNewNode(new SensorNode(HardwareConfiguration.HARDWARECONFIGURATION_1_PIR_1_LIDAR_LEDS, 2));
+            TransferNode(NewSensorViewbox, sensorViewboxes[1][1]);
+            AddNewNode(new SensorNode(HardwareConfiguration.HARDWARECONFIGURATION_2_PIR, 3));
+
+            
+
+            //SetNodeRotation(sensorViewboxes[1][1], NodeRotation.NODEROTATION_180);
+
+            //TransferNode(sensorViewboxes[1][1], sensorViewboxes[2][2]);
         }
+
+        public void AddNewNode(SensorNode node)
+        {
+            NewSensorViewbox.Child = null;
+            NewSensorViewbox.Child = node;
+            node.Button.Click += NodeClick;
+            NewNodeLabel.Content = "New Node!";
+            SetActiveViewbox(NewSensorViewbox);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void NodeClick(object sender, RoutedEventArgs e)
+        {
+            Viewbox source = null;
+
+            foreach (var list in sensorViewboxes)
+            {
+                foreach (var v in list)
+                {
+                    if(v.Child == null)
+                    {
+                        continue;
+                    }
+
+                    var node = v.Child as SensorNode;
+
+                    if(node.Button == sender)
+                    {
+                        source = v;
+                        break;
+                    }
+                }
+
+                if(source != null)
+                {
+                    break;
+                }
+            }
+
+            if(source == null)
+            {
+                var node = NewSensorViewbox.Child as SensorNode;
+
+                if (node.Button == sender)
+                {
+                    source = NewSensorViewbox;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Unknown Viewbox");
+                }
+            }
+
+            SetActiveViewbox(source);
+        }
+
+        private void SetActiveViewbox(Viewbox newActive)
+        {
+            if(newActive.Child == null)
+            {
+                throw new InvalidOperationException("A non occupied viewbox cannot be active");
+            }
+
+            foreach(var list in sensorViewboxes)
+            {
+                foreach(var v in list)
+                {
+                    v.Opacity = 0.5;
+                }
+            }
+
+            NewSensorViewbox.Opacity = 0.5;
+
+            ActiveViewbox = newActive;
+            ActiveViewbox.Opacity = 1.0f;
+
+            var node = ActiveViewbox.Child as SensorNode;
+
+            NodeNameLabel.Content = "Node " + node.NodeID;
+        }
+
+        private void TransferNode(Viewbox source, Viewbox destination)
+        {
+            SensorNode node = source.Child as SensorNode;
+
+            source.Child = null;
+            destination.Child = node;
+
+            if(source == NewSensorViewbox)
+            {
+                NewNodeLabel.Content = "";
+            }
+
+            SetNodeRotation(destination, node.rotation);
+            UpdatePulseStates();
+        }
+
+        private void SetNodeRotation(Viewbox viewbox, NodeRotation rotation)
+        {
+            double newAngle = 0;
+
+            switch(rotation)
+            {
+                case NodeRotation.NODEROTATION_0:
+                    newAngle = 0.0;
+                    break;
+                case NodeRotation.NODEROTATION_45:
+                    newAngle = 45.0;
+                    break;
+                case NodeRotation.NODEROTATION_90:
+                    newAngle = 90.0;
+                    break;
+                case NodeRotation.NODEROTATION_135:
+                    newAngle = 135.0;
+                    break;
+                case NodeRotation.NODEROTATION_180:
+                    newAngle = 180.0;
+                    break;
+                case NodeRotation.NODEROTATION_225:
+                    newAngle = 225.0;
+                    break;
+                case NodeRotation.NODEROTATION_270:
+                    newAngle = 270.0;
+                    break;
+                case NodeRotation.NODEROTATION_315:
+                    newAngle = 315.0;
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid Angle");
+            }
+
+            SensorNode node = viewbox.Child as SensorNode;
+            node.SetRotation(rotation);
+
+            var r = viewbox.RenderTransform as RotateTransform;
+            r.Angle = newAngle;
+        }
+
+        private void InitializePulses()
+        {
+            pulses = new List<List<PulsingCircle>>
+            {
+                new List<PulsingCircle>
+                {
+                    Pulse_0_0,
+                    Pulse_0_1,
+                    Pulse_0_2
+                },
+                new List<PulsingCircle>
+                {
+                    Pulse_1_0,
+                    Pulse_1_1,
+                    Pulse_1_2
+                },
+                new List<PulsingCircle>
+                {
+                    Pulse_2_0,
+                    Pulse_2_1,
+                    Pulse_2_2
+                }
+            };
+
+            foreach(var list in pulses)
+            {
+                foreach(var pulse in list)
+                {
+                    pulse.Button.Click += OnPulseClick;
+                }
+            }
+
+            UpdatePulseStates();
+        }
+
+        private void UpdatePulseStates()
+        {
+            int row = 0, column = 0;
+
+            for (row = 0; row < GridSize; ++row)
+            {
+                for (column = 0; column < GridSize; ++column)
+                {
+                    if (sensorViewboxes[row][column].Child == null)
+                    {
+                        pulses[row][column].Configure(10, 20, 3);
+                    }
+                    else
+                    {
+                        pulses[row][column].Configure(10, 10, 1);
+                    }
+                }
+
+            }
+        }
+
+        private void OnPulseClick(object sender, RoutedEventArgs e)
+        {
+            int row = 0, column = 0;
+            bool found = false;
+
+            for (row = 0; row < GridSize; ++row)
+            {
+                for (column = 0; column < GridSize; ++column)
+                {
+                    if(sender == pulses[row][column].Button)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(found)
+                {
+                    break;
+                }
+            }
+
+            if(ActiveViewbox != null && sensorViewboxes[row][column].Child == null)
+            {
+                TransferNode(ActiveViewbox, sensorViewboxes[row][column]);
+                SetActiveViewbox(sensorViewboxes[row][column]);
+            }
+            
+        }
+
+        private void InitializeViewboxes()
+        {
+            sensorViewboxes = new List<List<Viewbox>>
+            {
+                new List<Viewbox>
+                {
+                    SensorViewbox_0_0,
+                    SensorViewbox_0_1,
+                    SensorViewbox_0_2
+                },
+                new List<Viewbox>
+                {
+                    SensorViewbox_1_0,
+                    SensorViewbox_1_1,
+                    SensorViewbox_1_2
+                },
+                new List<Viewbox>
+                {
+                    SensorViewbox_2_0,
+                    SensorViewbox_2_1,
+                    SensorViewbox_2_2
+                }
+            };
+        }
+
+        private void RotateClockwise_Click(object sender, RoutedEventArgs e)
+        {
+            if(ActiveViewbox == null || ActiveViewbox.Child == null)
+            {
+                return;
+            }
+
+            var node = ActiveViewbox.Child as SensorNode;
+            var rotation = node.rotation;
+
+            switch(rotation)
+            {
+                case NodeRotation.NODEROTATION_0:
+                    rotation = NodeRotation.NODEROTATION_45;
+                    break;
+                case NodeRotation.NODEROTATION_45:
+                    rotation = NodeRotation.NODEROTATION_90;
+                    break;
+                case NodeRotation.NODEROTATION_90:
+                    rotation = NodeRotation.NODEROTATION_135;
+                    break;
+                case NodeRotation.NODEROTATION_135:
+                    rotation = NodeRotation.NODEROTATION_180;
+                    break;
+                case NodeRotation.NODEROTATION_180:
+                    rotation = NodeRotation.NODEROTATION_225;
+                    break;
+                case NodeRotation.NODEROTATION_225:
+                    rotation = NodeRotation.NODEROTATION_270;
+                    break;
+                case NodeRotation.NODEROTATION_270:
+                    rotation = NodeRotation.NODEROTATION_315;
+                    break;
+                case NodeRotation.NODEROTATION_315:
+                    rotation = NodeRotation.NODEROTATION_0;
+                    break;
+            }
+
+            SetNodeRotation(ActiveViewbox, rotation);
+        }
+
+        private void RotateCounterClockwise_Click(object sender, RoutedEventArgs e)
+        {
+            if (ActiveViewbox == null || ActiveViewbox.Child == null)
+            {
+                return;
+            }
+
+            var node = ActiveViewbox.Child as SensorNode;
+            var rotation = node.rotation;
+
+            switch (rotation)
+            {
+                case NodeRotation.NODEROTATION_0:
+                    rotation = NodeRotation.NODEROTATION_315;
+                    break;
+                case NodeRotation.NODEROTATION_45:
+                    rotation = NodeRotation.NODEROTATION_0;
+                    break;
+                case NodeRotation.NODEROTATION_90:
+                    rotation = NodeRotation.NODEROTATION_45;
+                    break;
+                case NodeRotation.NODEROTATION_135:
+                    rotation = NodeRotation.NODEROTATION_90;
+                    break;
+                case NodeRotation.NODEROTATION_180:
+                    rotation = NodeRotation.NODEROTATION_135;
+                    break;
+                case NodeRotation.NODEROTATION_225:
+                    rotation = NodeRotation.NODEROTATION_180;
+                    break;
+                case NodeRotation.NODEROTATION_270:
+                    rotation = NodeRotation.NODEROTATION_225;
+                    break;
+                case NodeRotation.NODEROTATION_315:
+                    rotation = NodeRotation.NODEROTATION_270;
+                    break;
+            }
+
+            SetNodeRotation(ActiveViewbox, rotation);
+        }
+
+        #endregion
+
+
     }
 }
