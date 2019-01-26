7cbc3b3295b6180b493b2aa1c120994cc8e1de58
Patrick Settle <patricksettle@hotmail.com>
2019-01-25 20:23:33 -0700

    Modified interface to match original implementation

--- nrf_firmware/src/sensor_algorithm/mm_activity_variables.c
+++ nrf_firmware/src/sensor_algorithm/mm_activity_variables.c
@@ -18,9 +18,6 @@ notes:
                         MACROS
 **********************************************************/
 
-/* Ease of access macro, optional usage. */
-#define AV(x, y)                ( activity_variables[(x) * MAX_AV_SIZE_X + (y)] )
-
 /**********************************************************
                           TYPES
 **********************************************************/
@@ -30,21 +27,18 @@ notes:
 **********************************************************/
 
 /**
-     Activity variable definition, can be accessed as AV(x, y).
+     Activity variable definition, can be accessed as AV_LOCAL(x, y).
 
      X is indexed left -> right. (In direction of North-American traffic)
      Y is indexed top -> bottom. (Moving away from the road)
 
-     Variable (x, y) is located at activity_variables[x * MAX_AV_SIZE_X + y]
+     Variable (x, y) is located at activity_variables[x * MAX_AV_LOCAL_SIZE_X + y]
 */
 static mm_activity_variable_t activity_variables[ACTIVITY_VARIABLES_NUM];
 
 /**********************************************************
                        DECLARATIONS
 **********************************************************/
-
-/* If AV outside min and max val, set to closest limit. */
-static void check_av_limits(uint8_t x, uint8_t y);
 
 /**********************************************************
                        DEFINITIONS
@@ -61,31 +55,7 @@ void mm_activity_variables_init(void)
     }
 }
 
-void mm_av_multiply(uint8_t x, uint8_t y, float factor)
+mm_activity_variable_t* mm_av_access(uint8_t x, uint8_t y)
 {
-    AV(x, y) *= factor;
-    check_av_limits(x, y);
-}
-
-mm_activity_variable_t mm_av_access(uint8_t x, uint8_t y)
-{
-    return AV(x, y);
-}
-
-void mm_av_set(uint8_t x, uint8_t y, mm_activity_variable_t val)
-{
-    AV(x, y) = val;
-    check_av_limits(x, y);
-}
-
-static void check_av_limits(uint8_t x, uint8_t y)
-{
-    if(AV(x, y) < ACTIVITY_VARIABLE_MIN)
-    {
-        AV(x, y) = ACTIVITY_VARIABLE_MIN;
-    }
-    else if(AV(x, y) > ACTIVITY_VARIABLE_MAX)
-    {
-        AV(x, y) = ACTIVITY_VARIABLE_MAX;
-    }
+    return &activity_variables[x * MAX_AV_SIZE_X + y];
 }
