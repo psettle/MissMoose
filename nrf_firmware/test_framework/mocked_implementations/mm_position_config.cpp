1f15939a3b9efdafccfbfb0eed01e23d741a2e57
Elijah Pennoyer <elijah.pennoyer@gmail.com>
2019-02-07 16:51:00 -0700

    Replaced all tabs with spaces Addressed merge comments

--- nrf_firmware/test_framework/mocked_implementations/mm_position_config.cpp
+++ nrf_firmware/test_framework/mocked_implementations/mm_position_config.cpp
@@ -19,7 +19,7 @@ extern "C" {
 /**********************************************************
                         CONSTANTS
 **********************************************************/
-#define MAX_NUMBER_NODES					( 9 )
+#define MAX_NUMBER_NODES                    ( 9 )
 
 
 /**********************************************************
@@ -43,109 +43,111 @@ static bool have_positions_changed = true;
 /* Initialize position configuration */
 void mm_position_config_init(void) 
 {
-	//Clear node_positions list, so that anything from the previous test is removed
+    have_positions_changed = true;
+
+    //Clear node_positions list, so that anything from the previous test is removed
     node_positions.clear();
 
-	//For now, hardcode inputting the 9 nodes of our network. 
+    //For now, hardcode inputting the 9 nodes of our network. 
 
     //Top left node
-	mm_node_position_t top_left;
-	top_left.node_id = 1;
-	top_left.node_type = HARDWARE_CONFIG_PIR_LIDAR_LED;
+    mm_node_position_t top_left;
+    top_left.node_id = 1;
+    top_left.node_type = HARDWARE_CONFIG_PIR_LIDAR_LED;
     top_left.node_rotation = NODE_ROTATION_90;
-	top_left.grid_position_x = -1;
-	top_left.grid_position_y = 1;
-	top_left.grid_offset_x = 0;
-	top_left.grid_offset_y = 0;
-	top_left.is_valid = true;
+    top_left.grid_position_x = -1;
+    top_left.grid_position_y = 1;
+    top_left.grid_offset_x = 0;
+    top_left.grid_offset_y = 0;
+    top_left.is_valid = true;
     node_positions.push_back(top_left);
 
-	mm_node_position_t top_middle;
-	top_middle.node_id = 2;
-	top_middle.node_type = HARDWARE_CONFIG_PIR_LIDAR_LED;
+    mm_node_position_t top_middle;
+    top_middle.node_id = 2;
+    top_middle.node_type = HARDWARE_CONFIG_PIR_LIDAR_LED;
     top_middle.node_rotation = NODE_ROTATION_180;
-	top_middle.grid_position_x = 0;
-	top_middle.grid_position_y = 1;
-	top_middle.grid_offset_x = 0;
-	top_middle.grid_offset_y = 0;
-	top_middle.is_valid = true;
+    top_middle.grid_position_x = 0;
+    top_middle.grid_position_y = 1;
+    top_middle.grid_offset_x = 0;
+    top_middle.grid_offset_y = 0;
+    top_middle.is_valid = true;
     node_positions.push_back(top_middle);
 
-	mm_node_position_t top_right;
-	top_right.node_id = 3;
-	top_right.node_type = HARDWARE_CONFIG_PIR_LIDAR_LED;
+    mm_node_position_t top_right;
+    top_right.node_id = 3;
+    top_right.node_type = HARDWARE_CONFIG_PIR_LIDAR_LED;
     top_right.node_rotation = NODE_ROTATION_180;
-	top_right.grid_position_x = 1;
-	top_right.grid_position_y = 1;
-	top_right.grid_offset_x = 0;
-	top_right.grid_offset_y = 0;
-	top_right.is_valid = true;
+    top_right.grid_position_x = 1;
+    top_right.grid_position_y = 1;
+    top_right.grid_offset_x = 0;
+    top_right.grid_offset_y = 0;
+    top_right.is_valid = true;
     node_positions.push_back(top_right);
 
-	mm_node_position_t middle_left;
-	middle_left.node_id = 4;
-	middle_left.node_type = HARDWARE_CONFIG_PIR_LIDAR;
+    mm_node_position_t middle_left;
+    middle_left.node_id = 4;
+    middle_left.node_type = HARDWARE_CONFIG_PIR_LIDAR;
     middle_left.node_rotation = NODE_ROTATION_90;
-	middle_left.grid_position_x = -1;
-	middle_left.grid_position_y = 0;
-	middle_left.grid_offset_x = 0;
-	middle_left.grid_offset_y = 0;
-	middle_left.is_valid = true;
+    middle_left.grid_position_x = -1;
+    middle_left.grid_position_y = 0;
+    middle_left.grid_offset_x = 0;
+    middle_left.grid_offset_y = 0;
+    middle_left.is_valid = true;
     node_positions.push_back(middle_left);
 
     mm_node_position_t middle_middle;
-	middle_middle.node_id = 5;
-	middle_middle.node_type = HARDWARE_CONFIG_PIR_PIR;
+    middle_middle.node_id = 5;
+    middle_middle.node_type = HARDWARE_CONFIG_PIR_PIR;
     middle_middle.node_rotation = NODE_ROTATION_0;
-	middle_middle.grid_position_x = 0;
-	middle_middle.grid_position_y = 0;
-	middle_middle.grid_offset_x = 0;
-	middle_middle.grid_offset_y = 0;
-	middle_middle.is_valid = true;
+    middle_middle.grid_position_x = 0;
+    middle_middle.grid_position_y = 0;
+    middle_middle.grid_offset_x = 0;
+    middle_middle.grid_offset_y = 0;
+    middle_middle.is_valid = true;
     node_positions.push_back(middle_middle);
 
     mm_node_position_t middle_right;
-	middle_right.node_id = 6;
-	middle_right.node_type = HARDWARE_CONFIG_PIR_PIR;
+    middle_right.node_id = 6;
+    middle_right.node_type = HARDWARE_CONFIG_PIR_PIR;
     middle_right.node_rotation = NODE_ROTATION_0;
-	middle_right.grid_position_x = 1;
-	middle_right.grid_position_y = 0;
-	middle_right.grid_offset_x = 0;
-	middle_right.grid_offset_y = 0;
-	middle_right.is_valid = true;
+    middle_right.grid_position_x = 1;
+    middle_right.grid_position_y = 0;
+    middle_right.grid_offset_x = 0;
+    middle_right.grid_offset_y = 0;
+    middle_right.is_valid = true;
     node_positions.push_back(middle_right);
 
     mm_node_position_t bottom_left;
-	bottom_left.node_id = 7;
-	bottom_left.node_type = HARDWARE_CONFIG_PIR_LIDAR;
+    bottom_left.node_id = 7;
+    bottom_left.node_type = HARDWARE_CONFIG_PIR_LIDAR;
     bottom_left.node_rotation = NODE_ROTATION_0;
-	bottom_left.grid_position_x = -1;
-	bottom_left.grid_position_y = -1;
-	bottom_left.grid_offset_x = 0;
-	bottom_left.grid_offset_y = 0;
-	bottom_left.is_valid = true;
+    bottom_left.grid_position_x = -1;
+    bottom_left.grid_position_y = -1;
+    bottom_left.grid_offset_x = 0;
+    bottom_left.grid_offset_y = 0;
+    bottom_left.is_valid = true;
     node_positions.push_back(bottom_left);
 
     mm_node_position_t bottom_middle;
-	bottom_middle.node_id = 8;
-	bottom_middle.node_type = HARDWARE_CONFIG_PIR_PIR;
+    bottom_middle.node_id = 8;
+    bottom_middle.node_type = HARDWARE_CONFIG_PIR_PIR;
     bottom_middle.node_rotation = NODE_ROTATION_90;
-	bottom_middle.grid_position_x = 0;
-	bottom_middle.grid_position_y = -1;
-	bottom_middle.grid_offset_x = 0;
-	bottom_middle.grid_offset_y = 0;
-	bottom_middle.is_valid = true;
+    bottom_middle.grid_position_x = 0;
+    bottom_middle.grid_position_y = -1;
+    bottom_middle.grid_offset_x = 0;
+    bottom_middle.grid_offset_y = 0;
+    bottom_middle.is_valid = true;
     node_positions.push_back(bottom_middle);
 
     mm_node_position_t bottom_right;
-	bottom_right.node_id = 9;
-	bottom_right.node_type = HARDWARE_CONFIG_PIR_LIDAR;
+    bottom_right.node_id = 9;
+    bottom_right.node_type = HARDWARE_CONFIG_PIR_LIDAR;
     bottom_right.node_rotation = NODE_ROTATION_270;
-	bottom_right.grid_position_x = 1;
-	bottom_right.grid_position_y = -1;
-	bottom_right.grid_offset_x = 0;
-	bottom_right.grid_offset_y = 0;
-	bottom_right.is_valid = true;
+    bottom_right.grid_position_x = 1;
+    bottom_right.grid_position_y = -1;
+    bottom_right.grid_offset_x = 0;
+    bottom_right.grid_offset_y = 0;
+    bottom_right.is_valid = true;
     node_positions.push_back(bottom_right);
 
 }
@@ -153,7 +155,7 @@ void mm_position_config_init(void)
 /* Gets the entire array of node positions for the system */
 mm_node_position_t const * get_node_positions(void) 
 { 
-	return node_positions.data();
+    return node_positions.data();
 }
 
 /* Gets a specific node position by node id. Returns NULL if
@@ -161,15 +163,15 @@ mm_node_position_t const * get_node_positions(void)
  */
 mm_node_position_t const * get_position_for_node(uint16_t node_id) 
 { 
-    for (auto& position : node_positions)
+    for (auto const & position : node_positions)
     {
         if(position.node_id == node_id && position.is_valid)
-		{
+        {
             return &position;
         }
     }
 
-	return NULL;
+    return NULL;
 }
 
 /* Gets a specific node position by node location. Returns NULL if
@@ -177,34 +179,34 @@ mm_node_position_t const * get_position_for_node(uint16_t node_id)
  */
 mm_node_position_t const * get_node_for_position(int8_t x, int8_t y) 
 { 
-    for (auto& position : node_positions)
-	{
-		if( !position.is_valid )
-		{
-			continue;
-		}
+    for (auto const & position : node_positions)
+    {
+        if( !position.is_valid )
+        {
+            continue;
+        }
 
-		if(position.grid_position_x != x)
-		{
-			continue;
-		}
+        if(position.grid_position_x != x)
+        {
+            continue;
+        }
 
-		if(position.grid_position_y != y)
-		{
-			continue;
-		}
+        if(position.grid_position_y != y)
+        {
+            continue;
+        }
 
-		return &position;
-	}
+        return &position;
+    }
 
-	return NULL;
+    return NULL;
 }
 
 /* Gets the current number of nodes whose positions have been configured. */
 uint16_t get_number_of_nodes(void) 
 { 
     uint16_t number_of_nodes = 0;
-    for (auto& position : node_positions)
+    for (auto const & position : node_positions)
     {
         if(position.is_valid)
         {
@@ -212,7 +214,7 @@ uint16_t get_number_of_nodes(void)
         }
     }
 
-	return number_of_nodes;
+    return number_of_nodes;
 }
 
 /* Determines if the node positions have changed since the last read.
@@ -233,5 +235,5 @@ bool have_node_positions_changed(void)
  * making redundant checks on node position data.*/
 void clear_unread_node_positions(void) 
 {
-	have_positions_changed = false;
+    have_positions_changed = false;
 }
